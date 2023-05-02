a13631b25ea4152a1b8b275e79fae6f0c6f571c9
Katharine Zarbock <kjzarbock@gmail.com>
2023-05-02 14:31:31 -0500

    Final

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,10 +1,16 @@
-import { harvestPlants } from "./harvester.js";
+import { createPlan } from "./plan.js";
+import { addPlant } from "./field.js";
+import { plantSeeds } from "./tractor.js";
+import { usePlants } from './field.js';
+import { harvestPlants } from './harvester.js';
 
-export const Catalog = (harvestPlants) => {
-    const mainElement = document.querySelector('main');
-    harvestedFood.forEach((food) => {
-      const foodItem = `<section class="plant">${food.name}</section>`;
-      mainElement.innerHTML += foodItem;
-    });
-  };
-  
+
+export const catalog = () => {
+  const foods = harvestPlants(usePlants());
+  let htmlString = '<main class="container">';
+  for (const food of foods) {
+    htmlString += `<section class="plant">${food.type}</section>`;
+  }
+  htmlString += '</main>';
+  return htmlString;
+};
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { addPlant } from "./field.js";
 import { plantSeeds } from "./tractor.js";
 import { usePlants } from './field.js';
 import { harvestPlants } from './harvester.js';
+import { catalog } from './catalog.js';
 
 const yearlyPlan = createPlan();
 plantSeeds(yearlyPlan);
@@ -11,3 +12,9 @@ console.log(usePlants());
 
 const test = harvestPlants(usePlants());
 console.log(test);
+
+const htmlFood = document.querySelector(".container");
+htmlFood.innerHTML = catalog()
+for (const iterator of harvestPlants(usePlants())) {
+    console.log(iterator)
+}
